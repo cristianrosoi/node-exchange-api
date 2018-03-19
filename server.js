26a6ec8ea32b883ff187a5306f16924d98fdf2b5
Cristian Rosoi <cristian.rosoi@gmail.com>
2018-03-19 21:47:05 +0200

    added app.json, Procfile, ready for Heroku

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const app = express();
 const init = require('./src/app/index');
 const getCurrencyFromInterval = require('./src/app/controller/getCurrencyInterval');
 
-app.get('/api/:date/:currency', (req, res) => { 
+app.set('port', (process.env.PORT || 8000));
+
+app.get('/api/:date/:currency', (req, res) => {
     let date = req.params.date;
     let currency = req.params.currency;
-    
+
     init(date, currency, function(result) {
-        res.send(result); 
+        res.send(result);
     });
 });
 
@@ -16,14 +18,14 @@ app.get('/api/:startDate/:endDate/:currency', (req, res) => {
     let startDate = req.params.startDate;
     let endDate = req.params.endDate;
     let currency = req.params.currency;
-    
+
     getCurrencyFromInterval(startDate, endDate, currency, function(result){
         res.send(result);
     })
 })
 
 
-app.listen(8000, () => {
+app.listen(app.get('port'), () => {
     console.log('Loading...');
     console.log('Example app listening on port 8000!');
     console.log('Go to: http://localhost/api/{date}/{currency}');
